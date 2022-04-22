340272194368266417e4def736c1a2b595ad57da
hom-bahrani <hab0184@gmail.com>
2022-04-22 16:32:48 +0100

    Initial commit

---
+++ test/tdd-spark-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TddSparkAws from '../lib/tdd-spark-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tdd-spark-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TddSparkAws.TddSparkAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
