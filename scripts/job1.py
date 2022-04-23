57323be5b2b3d0213e59755f2a9f45308b3dd010
hom-bahrani <hab0184@gmail.com>
2022-04-23 08:52:51 +0100

    update spark script

--- scripts/job1.py
+++ scripts/job1.py
@@ -1,44 +1,33 @@
 import sys
-from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame
 from awsglue.job import Job
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
-sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
+glueContext = GlueContext(SparkContext.getOrCreate())
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Script generated for node Amazon S3
-dynamic_frame_read = glueContext.create_dynamic_frame.from_catalog(
-    database="customer_database",
-    table_name="customers_csv",
-    transformation_ctx="dynamic_frame_read",
-)
+# Data Catalog: database and table name
+db_name = "customer_database"
+tbl_name = "customers_csv"
+output_dir = "s3://appflow-test-ash/glue/data/customers_database/customers_json/"
 
-data_frame = dynamic_frame_read.toDF()
+# Read data into a DynamicFrame using the Data Catalog metadata
+customer_df = glueContext.create_dynamic_frame.from_catalog(database = db_name, table_name = tbl_name)
 
-# Script generated for node Rename Field
-dataframe_changed_field = data_frame.apply(
-    frame=dynamic_frame_read,
-    old_name="customerid",
-    new_name="customerUniqueID",
-    transformation_ctx="dataframe_changed_field",
-)
+# Remove erroneous records
+customer_df = customer_df.toDF()
+customer_df = customer_df.where("customerid is NOT NULL")
 
-# Script generated for node Amazon S3
-S3_frame = glueContext.write_dynamic_frame.from_options(
-    frame=dataframe_changed_field,
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "path": "s3://appflow-test-ash/glue/data/customers_database/customer_json/",
-        "partitionKeys": [],
-    },
-    transformation_ctx="S3_frame",
-)
+# Remove the namestyle column
+customer_df.drop('namestyle')
+
+# write output to S3
+glueContext.write_dynamic_frame.from_options(frame = customer_df, connection_type = "s3", connection_options = {"path": output_dir}, format = "json")
 
 job.commit()
